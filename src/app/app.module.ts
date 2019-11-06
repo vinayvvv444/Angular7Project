99afc3317a59f5577044934f56c4f7a3f079ac1e
vinay valsan <vinayvvv444@gmail.com>
2019-11-07 00:19:01 +0400

    jwt authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule,FormsModule } from "@angular/forms";
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 
@@ -11,13 +11,16 @@ import { UserComponent } from './user/user.component';
 import { RegistrationComponent } from './user/registration/registration.component';
 import { UserService } from './shared/user.service';
 import { LoginComponent } from './user/login/login.component';
+import { HomeComponent } from './home/home.component';
+import { AuthInterceptor } from './auth/auth.interceptor';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
     RegistrationComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -30,7 +33,11 @@ import { LoginComponent } from './user/login/login.component';
     }),
     FormsModule
   ],
-  providers: [UserService],
+  providers: [UserService,{
+    provide:HTTP_INTERCEPTORS,
+    useClass:AuthInterceptor,
+    multi:true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/shared/user.service';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -10,9 +14,26 @@ formModel={
   UserName:'',
   Password:''
 }
-  constructor() { }
+  constructor(private service:UserService,private router:Router,private toastr:ToastrService) { }
 
   ngOnInit() {
+    if(localStorage.getItem('token')!= null)
+    this.router.navigateByUrl('/home');
   }
 
+  onSubmit(form:NgForm)
+  {
+this.service.login(form.value).subscribe(
+  (res:any) =>{
+    localStorage.setItem('token',res.token);
+    this.router.navigateByUrl('/home');
+  },
+  err =>{
+    if(err.status == 400)
+this.toastr.error('Incorrect username or password','Authentication Failed');
+else
+console.log(err);
+  }
+);
+  }
 }
